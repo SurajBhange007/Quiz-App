7e0a19ae3148c3b270601aee0d55565237bf0631
SurajBhange007 <80091812+SurajBhange007@users.noreply.github.com>
2023-04-05 14:16:16 +0530

    Added files

---
+++ end.js
@@ -0,0 +1,28 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+const finalScore = document.getElementById('finalScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+const MAX_HIGH_SCORES= 5;
+
+finalScore.innerText = mostRecentScore;
+username.addEventListener("keyup", ()=>{
+    saveScoreBtn.disabled = !username.value;    
+})
+
+saveHighScore= (event)=>{
+    event.preventDefault();
+
+    const score ={
+        score : mostRecentScore,
+        name : username.value
+    }
+    highScores.push(score);
+    highScores.sort( (a, b)=> b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/')
+}
---
+++ game.js
@@ -0,0 +1,103 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader= document.getElementById('loader');
+const game = document.getElementById('game');
+
+let currentQuestion = {};
+let acceptingAnswer = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple").then(res =>{
+    return res.json();
+}).then(loadedQuestion=>{
+    questions = loadedQuestion.results.map(questions=>{
+      console.log(questions);  
+        const formattedQuestion = {
+            question : questions.question
+        };
+        const answerChoices = [...questions.incorrect_answers]
+        formattedQuestion.answer = Math.floor(Math.random()*3)+1;
+        answerChoices.splice(formattedQuestion.answer -1 , 0, questions.correct_answer)
+
+        answerChoices.forEach((choice, index)=>{
+            formattedQuestion["choice" + (index+1)] = choice;
+        })
+        return formattedQuestion;
+    })  
+
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
+    startGame();
+}).catch(err=>{
+    console.error(err);
+})
+
+//constant 
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 10;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length ===0 || questionCounter>=MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score);
+        //go to end page
+        return window.location.assign("/end.html")
+    }
+    questionCounter++;
+    progressText.innerText = `Question: ${questionCounter}/${MAX_QUESTIONS}`
+    // update progress bar
+    progressBarFull.style.width  = `${(questionCounter/MAX_QUESTIONS)*100}%`;
+
+    const questionIndex = Math.floor(Math.random()*availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice=>{
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice'+number];
+    })
+
+    availableQuestions.splice(questionIndex , 1);
+    acceptingAnswer = true;
+}
+
+choices.forEach(choice =>{
+    choice.addEventListener('click', e=>{
+        if(!acceptingAnswer)return ;
+        acceptingAnswer = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply = ( selectedAnswer == currentQuestion.answer )  ?  'correct' : 'incorrect';
+        
+        selectedChoice.parentElement.classList.add(classToApply);
+        if(classToApply === 'correct'){
+            incrementScore(CORRECT_BONUS);
+        }
+        setTimeout(()=>{
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 400);
+        
+    })
+})
+
+
+incrementScore= num=>{
+    score+=num;
+    scoreText.innerText = score;
+}
